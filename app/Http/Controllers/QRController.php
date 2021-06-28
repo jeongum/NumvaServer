3eaf47f75a88f6d29398753cd2fb3af636ffc964
leahJU <aldlswjddma@naver.com>
2021-06-28 08:43:24 +0000

    QR generator and QR redirection

--- app/Http/Controllers/QRController.php
+++ app/Http/Controllers/QRController.php
@@ -4,12 +4,38 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
+use App\Models\QRData;
 
 class QRController extends Controller
 {
     /* Show QR View to Scanner */
     public function index(){
         $qr_id = Session::get('qr_id');
-        dd($qr_id);
+        $qrcode = QRData::where('qr_id', $qr_id)->first();
+        
+        return view('qr/service', compact('qrcode'));
+    }
+    
+    /* Generate QR */
+    public function generateQRCode(){
+        $qr_id = random_int(100000,999999);
+        while(QRData::where('qr_id', $qr_id)->exists()){
+            $qr_id = random_int(100000,999999);
+        }
+        $qr_path = 'http://ec2-13-125-250-97.ap-northeast-2.compute.amazonaws.com/qr/'.$qr_id;
+        $img_path = 'img/qr/'.$qr_id.'.png';
+       
+        QrCode::format('png')
+                ->size(500)
+                ->merge('/public/img/logo_purple_with_bg.png', .2)
+                ->generate($qr_path, public_path($img_path));
+        
+        QRData::create([
+            "qr_id" => $qr_id,
+            "url" => $img_path
+        ]);
+        
+        return view('qr/generate');
     }
 }
---
+++ resources/views/qr/generate.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.master')
+
+@section('content')
+
+<div id="main">
+	<div id="main-bg"></div>
+    <div class="container">
+		<div class="intro">
+			<p class="mb-0">차주용</p>
+			<p class="mb-0">개인정보보호 서비스</p>
+			<p class="main-numba">NUMVA</p>
+		</div>
+		<div class="mini-nav row justify-content-center">
+			<a>ABOUT</a>
+			<a>HOWTOUSE</a>
+			<a>REVIEW</a>
+		</div>
+	</div>
+</div>
+
+@endsection
